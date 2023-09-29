1c7d35239fa1019bec18b0ff6290b1486da5858e
WuXiangChen <31811788+WuXiangChen@users.noreply.github.com>
2023-09-29 14:21:57 +0800

    Update Evaluation_process_design.py

--- EarlyPR-trainingProcedure/generate_TestData/Evaluation_process_design.py
+++ EarlyPR-trainingProcedure/generate_TestData/Evaluation_process_design.py
@@ -1,7 +1,4 @@
-'''
-    本节的目标是建立pr_related的评估过程
-'''
-'''导包区'''
+
 from statistic_PR_ForREPO import PR_related_mian
 import pandas as pd
 import os
@@ -16,16 +13,13 @@ from tqdm import tqdm
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 
-# 1. 进行筛选，确定待选的fork名，通过Cm仓库其中所有的commits全部拿下来，以sha作为存储标志
 def selectForksFromCmRepository():
     owners = ['VSChina',  'alikins']
     return owners
 
-# 2. 通过PR仓库将指定fork名下的所有commits全部拿下来，以sha作为存储标志
 def getCommitsFromPRRepository(owners):
     PR_related_mian(owners)
 
-# 3. 利用以上信息做标签 在PR下的commit集合全部置1，做正样本；反之，置0做负样本
 def createLabelsForCommits():
     root_path = "Test_PR_Data_Related/_1_Commits_pSpecificFork/*.csv"
     dir_ls = glob.glob(root_path)
@@ -43,12 +37,10 @@ def createLabelsForCommits():
         df_N = pd.DataFrame([diff_commits,negative_Cm]).T
         df_data = pd.concat([df_P,df_N],axis=0).reset_index(drop=True)
         df_data.columns = ["testD","label"]
-        # 至此建立完了正负样本
         accuracy, recall, precision, specificity, FPR = performPredictProcess(df_data,path)
         result.append([accuracy, recall, precision, specificity, FPR])
     return result
 
-# 4. 在进行预测的时候，建立搜索过程
 def expand_comRE_TO_TestD(com_re,len_x,len_):
     result = [0]*len_x
     indices = [index for index, element in enumerate(com_re) if element == 1]
@@ -75,15 +67,12 @@ def performPredictProcess(df_data,file_):
         j += 1
         len_+=1
         com_re = []
-        # 接下来就是挨个进行查询与预测工作了
         com = list(x_testD[0:len_])
         true_removed = []
         i = 0
         while i<len(x_testD):
             statistcsCom = SearchStaForCommitCom(com,query,repo_owner)
             if len(statistcsCom)!=0:
-                # 加载模型进行判断
-                # CommitsNum	PR_FilesChangedNum	Churn_Num	diff_FilesChangedRate	TS_entropy	TS_INTwoCommits	label
                 obj = statistcsCom[0]
                 tmp_testD = np.asarray([obj['CommitsNum'],obj['PR_FilesChangedNum'],obj['Churn_Num'],
                              obj['diff_FilesChangedRate'],obj['TS_entropy'],obj['TS_INTwoCommits']]).reshape((1,-1))
@@ -93,7 +82,6 @@ def performPredictProcess(df_data,file_):
                 print("查询结果为空"+str(i))
                 tmp_ypred = 0
                 com_re.extend([tmp_ypred])
-            # 当一次判断中tmp_ypred为1时，去除该组合对应在x_testD中的值，并且修正com的指向
             if tmp_ypred==1:
                 true_removed.extend([i for i in range(i,i+len_)])
                 i += len_
@@ -104,9 +92,6 @@ def performPredictProcess(df_data,file_):
                 if i + len_>len(x_testD):
                     break
                 com = list(x_testD[i:i + len_])
-        # 跑通一轮 进行简单的预测 以及 遍历剩下的集合
-        # 这里封装着所有的预测结果, 但是还是要处理一下的
-        # 由com_re的结果拓展到全结果上
         if j>1:
             com_re = expand_comRE_TO_TestD(com_re,len(x_testD),len_)
 
@@ -125,18 +110,11 @@ def performPredictProcess(df_data,file_):
 
         y_indices = y_test_copy.index[true_removed]
         y_test_copy = y_test_copy.drop(y_indices)
-        # print("*************************")
-        # print("y_pred_final_的累次变化")
-        # print(y_pred_final.value_counts())
-        # print("*************************")
-        # print(j)
 
     print("____final____")
     return  calculateTop1Accuracy(y_pred_final.iloc[:,0].tolist(), y_test.tolist())
 
 
-# 4.1 首先对其中每一个commit进行搜索，对搜索结果进行无放回的采样，
-# 4.2 接着，对剩下的commits进行按序组合，重复以上工作
 def SearchStaForCommitCom(test_com,query,repo_owner,suffix="commits"):
     coll_names = database.list_collection_names()
     repo_name = repo_owner["repo"]
@@ -160,21 +138,18 @@ def SearchStaForCommitCom(test_com,query,repo_owner,suffix="commits"):
         obj = query_result[0]
         commits_Time = obj.get('Commtis_Time')
         if commits_Time is not None:
-            # 这边需要根据时间列表计算熵值
             entropy = calculate_entropy(commits_Time)
             del obj['Commtis_Time']
             obj["TS_entropy"] = entropy[0]
 
     else:
         #print(repla_query)
-        print("query_result_查询错误"+str(test_com))
+        print("query_result"+str(test_com))
         return []
 
 
     return [obj]
 
-
-# 5. 设计评估结果：
 # 5.1 top 1 accuracy
 # 5.2 top 5 accuracy
 def calculateTop1Accuracy(y_pred,y_test):
@@ -184,23 +159,11 @@ def calculateTop1Accuracy(y_pred,y_test):
     FP = cm[0, 1]
     FN = cm[1, 0]
     TP = cm[1, 1]
-    # 计算特异度
     specificity = TN / (TN + FP)
-    # 计算假阳性率
     FPR = FP / (FP + TN)
-    # 计算精确度
     precision = TP / (TP + FP)
-    # 计算召回率
     recall = TP / (TP + FN)
-    # 计算预测准确率
     accuracy = accuracy_score(y_test, y_pred)
-
-    print("特异度:", specificity)
-    print("假阳性率:", FPR)
-    print("精确度:", precision)
-    print("召回率:", recall)
-    print("Accuracy:", accuracy)
-    print("===================================================")
 
     return accuracy, recall, precision, specificity, FPR
 
