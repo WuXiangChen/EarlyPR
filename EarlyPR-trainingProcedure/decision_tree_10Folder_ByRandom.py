24c311283f6847ed27fa6824ceb71a285604708b
WuXiangChen <31811788+WuXiangChen@users.noreply.github.com>
2023-09-29 14:19:46 +0800

    Update decision_tree_10Folder_ByRandom.py

--- EarlyPR-trainingProcedure/decision_tree_10Folder_ByRandom.py
+++ EarlyPR-trainingProcedure/decision_tree_10Folder_ByRandom.py
@@ -14,39 +14,26 @@ import os
 import joblib
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
-# 首先 按照group_Id进行十折划分
-# 然后 判断划分后的数据是否正负样本均衡，是否比例均衡
-# 最后预测的时候，进行 cherry-pick检测验证
 
 def evaluate_model_three(y_test, y_pred):
-    # 计算混淆矩阵
     cm = confusion_matrix(y_test, y_pred)
-    # 计算预测准确率
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred,average='macro')
     recall = recall_score(y_test, y_pred,average='macro')
     f1_score_ = f1_score(y_test, y_pred,average='macro')
-    # 返回评估指标结果
     return accuracy, recall, precision, f1_score_, np.array(cm)
 
 def evaluate_model_two(y_test, y_pred):
-    # 计算混淆矩阵
     cm = confusion_matrix(y_test, y_pred)
     TN = cm[0, 0]
     FP = cm[0, 1]
     FN = cm[1, 0]
     TP = cm[1, 1]
-    # 计算特异度
     specificity = TN / (TN + FP)
-    # 计算假阳性率
     FPR = FP / (FP + TN)
-    # 计算精确度
     precision = TP / (TP + FP)
-    # 计算召回率
     recall = TP / (TP + FN)
-    # 计算预测准确率
     accuracy = accuracy_score(y_test, y_pred)
-    # 返回评估指标结果
     f1_score_ = f1_score(y_test, y_pred)
     return accuracy, recall, precision, specificity, FPR, f1_score_, np.array(cm)
 
@@ -55,45 +42,33 @@ def print_evaluation_(param):
         print(f"{key}: {value}")
 
 def train_and_evaluate_decision_tree_model(data_file):
-    # 加载数据集
     print(data_file)
     data = pd.read_csv(data_file)
     data['mergedPR'].fillna(-1, inplace=True)
-    # 删除含有缺失值的行
     data.dropna(inplace=True)
-    # data.drop("#Churn", inplace=True, axis=1)
-    # data['mergedPR'].fillna(-1, inplace=True)
-    # data = data[data["label"]==1]
-    # data["pr_1Mon_files_Kth_JS"] = data["pr_1Mon_files_Kth_JS"].map(eval).map(lambda x: max(x))
-    # data.reset_index(inplace=True,drop=True)
-
-    # 分割特征和标签
-    # 更换mergedPR测试
     X = data.iloc[:, 4:]
     y = data.iloc[:, 1]
 
     #y = pd.Series(le.fit_transform(y))
     classes = len(y.unique())
 
-    # 计算 1 和 0 的数量
     num_ones = y[y == 1].count()
     num_zeros = y[y == 0].count()
     num_Mone = y[y == -1].count()
     print("Number of 1's:", num_ones)
     print("Number of 0's:", num_zeros)
     print("Number of -1's:", num_Mone)
-    # # 处理缺失值
-    # imputer = SimpleImputer(strategy='mean')
-    # X = imputer.fit_transform(X)
+
+
     X.reset_index(inplace=True)
     group_id = X["index"].sample(frac=1, random_state=42).reset_index(drop=True)
     len_ = int(len(group_id)/10)
     group_test_IDindex = [group_id[i*len_:(i+1)*len_] for i in range(10)]
-    # 创建决策树分类器对象
+
     clf = RandomForestClassifier()
     pro_all = []
     i = 0
-    # 将数据集拆分为训练集和测试集
+
     for test_groupId in group_test_IDindex:
         test_index = pd.Series(X[X["index"].isin(test_groupId)].index)
         train_index = pd.Series(X[~X["index"].isin(test_groupId)].index)
@@ -104,13 +79,9 @@ def train_and_evaluate_decision_tree_model(data_file):
         train_x = X.loc[train_index].iloc[:,2:]
         train_y = y.loc[train_index]
 
-        # 使用训练集对模型进行训练
+
         clf.fit(train_x, train_y)
-        # 在这里完成它的Sequential predict过程
-        # 对测试集进行预测
         y_pred = clf.predict(test_x)
-        # 计算混淆矩阵
-        # print(i)
         i+=1
         y_test = test_y.to_numpy()
         pro_all.append([y_test, y_pred])
