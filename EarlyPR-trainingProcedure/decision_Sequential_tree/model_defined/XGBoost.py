d05a0d6eb643d738f55b4ec77f9577bb0a6dc812
WuXiangChen <31811788+WuXiangChen@users.noreply.github.com>
2023-09-29 14:09:06 +0800

    Update XGBoost.py

--- EarlyPR-trainingProcedure/decision_Sequential_tree/model_defined/XGBoost.py
+++ EarlyPR-trainingProcedure/decision_Sequential_tree/model_defined/XGBoost.py
@@ -1,4 +1,3 @@
 import xgboost as xgb
 
 xgb_trainer = xgb.XGBClassifier(objective='binary:logistic', n_estimators=100, learning_rate=0.1, max_depth=5)
-#xgb_trainer = xgb.XGBClassifier(objective='multi:softmax', num_class=3, n_estimators=100, learning_rate=0.1, max_depth=5)
