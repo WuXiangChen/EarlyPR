63c5e4bbe4bbaf88d1623c0869cf32181eae4780
WuXiangChen <31811788+WuXiangChen@users.noreply.github.com>
2023-09-29 14:24:43 +0800

    Update MongoDB_utils.py

--- EarlyPR-trainingProcedure/generate_TestData/MongoDB_Related/MongoDB_utils.py
+++ EarlyPR-trainingProcedure/generate_TestData/MongoDB_Related/MongoDB_utils.py
@@ -1,13 +1,9 @@
 
-'''导包区'''
 from pymongo import MongoClient
 from generate_TestData.MongoDB_Related.CONSTANT import mongodb_url,database_name
 from pymongo.errors import CollectionInvalid
 import json
-# 数据库连接
-# 连接到MongoDB
 
-'''连接MongoDB'''
 def mongo_conn():
     client = MongoClient(mongodb_url)
     database = client[database_name]
@@ -33,33 +29,23 @@ class RedisStorage:
     def __init__(self, host='localhost', port=6379, db=0):
         self.redis_client = redis.StrictRedis(host=host, port=port, db=db)
     def set_key_value(self, key, value):
-        """
-        存储键值对到Redis数据库
-        """
+
         return self.redis_client.set(key, value)
 
     def get_value(self, key):
-        """
-        获取指定键的值
-        """
+
         return self.redis_client.get(key)
 
     def delete_key(self, key):
-        """
-        删除指定的键
-        """
+
         return self.redis_client.delete(key)
 
     def exists_key(self, key):
-        """
-        检查键是否存在
-        """
+
         return self.redis_client.exists(key)
 
     def append_value(self, key, value):
-        """
-        将值追加到已有键的值中
-        """
+
         old_values = self.get_value(key)
         old_values_dict = {}
         if old_values!=None and json.loads(old_values)!=None and isinstance(json.loads(old_values),dict):
